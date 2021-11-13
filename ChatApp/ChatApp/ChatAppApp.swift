b7777b7fec23495f256668e000fcb8b5e83d9dc6
sahil-sahu-dev <80636783+sahil-sahu-dev@users.noreply.github.com>
2021-11-13 16:42:02 +0530

    Add files via upload

---
+++ ChatApp/ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Sahil Sahu on 13/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
