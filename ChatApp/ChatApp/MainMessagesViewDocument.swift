68bf5a732a7668bba6e3c27706a346e62aa3f4ae
sahil-sahu-dev <sahilsahucode@gmail.com>
2021-11-25 05:30:48 +0530

    main messages vie model. Fetching current user data

--- ChatApp/ChatApp/MainMessagesViewDocument.swift
+++ ChatApp/ChatApp/MainMessagesViewDocument.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+
+class MainMessagesViewDocument: ObservableObject {
+    
+    @Published var chatUser: ChatUser?
+    @Published var errorMessage: String = ""
+    
+    
+    init() {
+        fetchCurrentUser()
+    }
+    
+    private func fetchCurrentUser() {
+        
+        guard let user_uid = FirebaseManager.shared.auth.currentUser?.uid else {
+            self.errorMessage = "Could not find user uid"
+            return
+        }
+        
+        FirebaseManager.shared.firestore.collection("users").document(user_uid).getDocument { snapshot, error in
+            
+            if let error = error {
+                self.errorMessage = "Could not fetch user data \(error)"
+                print("Couldnt fetch user ")
+            }
+            
+            guard let data = snapshot?.data() else {
+                self.errorMessage = "No data found"
+                return
+            }
+            
+            let uid = data["uid"] as? String ?? ""
+            let email = data["email"] as? String ?? ""
+            let profileImageUrl = data["profileImageUrl"]  as? String ?? ""
+            
+            self.chatUser = ChatUser(uid: uid, imageProfile: profileImageUrl, email: email)
+        }
+        
+    }
+}
