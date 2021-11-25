c653d070d3e8d235b6394b4731a719298519f50d
sahil-sahu-dev <sahilsahucode@gmail.com>
2021-11-25 05:30:14 +0530

    chat user model

---
+++ ChatApp/ChatApp/ChatUser.swift
@@ -0,0 +1,17 @@
+//
+//  ChatUser.swift
+//  ChatApp
+//
+//  Created by Sahil Sahu on 25/11/21.
+//
+
+import Foundation
+
+
+struct ChatUser {
+    
+    let uid: String
+    let imageProfile: String
+    let email: String
+    
+}
---
+++ ChatApp/ChatApp/MainMessagesViewDocument.swift
@@ -0,0 +1,8 @@
+//
+//  MainMessagesViewDocument.swift
+//  ChatApp
+//
+//  Created by Sahil Sahu on 25/11/21.
+//
+
+import Foundation
