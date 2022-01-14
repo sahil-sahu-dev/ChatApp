7747914515437c4daf7817326bb13ac1cc6ebd68
sahil-sahu-dev <sahilsahucode@gmail.com>
2022-01-14 09:10:09 +0530

    show recent messages

---
+++ ChatApp/ChatApp/RecentMessage.swift
@@ -0,0 +1,39 @@
+//
+//  RecentMessage.swift
+//  ChatApp
+//
+//  Created by Sahil Sahu on 14/01/22.
+//
+
+import Foundation
+import Firebase
+
+struct RecentMessage: Identifiable {
+    var id: String {documentId}
+    let documentId: String
+    let text, toId, fromId : String
+    let timestamp: Timestamp
+    let email: String
+    let profileImageUrl: String
+    
+    init(documentId: String, data:[String: Any]) {
+        self.documentId = documentId
+        self.text = data["Text"] as? String ?? ""
+        self.fromId = data["fromId"] as? String ?? ""
+        self.toId = data["toId"] as? String ?? ""
+        self.profileImageUrl = data["profileImageUrl"] as? String ?? ""
+        self.email = data["email"] as? String ?? ""
+        self.timestamp = data["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+    }
+   
+    var timeAgo: String {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.unitsStyle = .abbreviated
+        return formatter.localizedString(for: timestamp.dateValue(), relativeTo: Date())
+    }
+    
+    var username: String {
+        return email.components(separatedBy: "@").first ?? email
+    }
+}
+
